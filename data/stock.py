5d66652d1535cd9e7c61e415b39206ef84d31ab4
Ignacio Martin Alliati <ialliati01@qub.ac.uk>
2023-12-07 11:01:26 +0000

    DB support

--- data/stock.py
+++ data/stock.py
@@ -64,8 +64,13 @@ def update_write_db(ticker,in_dates):
     _d1 = in_dates[0].strftime('%Y-%m-%d')
     _d2 = in_dates[1].strftime('%Y-%m-%d')
     _data = pd.read_sql(f"SELECT * FROM {ticker} WHERE Date BETWEEN '{_d1}' AND '{_d2}'",engine)
+    _data.Date = pd.to_datetime(_data.Date)
     return _data
 
 if __name__ == "__main__":
     df = get_stock_data('TSLA','20201127','20201212')
     print(df)
+    print(type(df.Date[0]))
+    dd = get_stock_data('TSLA','20201127','20201220')
+    print(dd)
+    print(type(dd.Date[0]))
